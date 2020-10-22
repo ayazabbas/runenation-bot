b44c1c49c526187ef9b1a520ad61aeef16cc0a07
Ayaz Abbas# <ayaz38@live.co.uk>
2020-10-22 15:36:56 +0100

    Upgrade python version

--- bot/config.py
+++ bot/config.py
@@ -5,9 +5,6 @@ load_dotenv()
 ENV = os.getenv("ENV")
 S3_BUCKET = os.getenv("CONFIG_S3_BUCKET")
 GOOGLE_SECRET_S3_KEY = os.getenv("GOOGLE_SECRET_S3_KEY")
-if ENV == "dev":
-    TOKEN = os.getenv("DISCORD_TOKEN_DEV")
-else:
-    TOKEN = os.getenv("DISCORD_TOKEN")
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 # download client_secret.json for authenticating with Google API
