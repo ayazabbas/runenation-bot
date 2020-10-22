64a1f5182f55a23fa3b8ff3f22518fad8e94b55b
Ayaz Abbas# <ayaz38@live.co.uk>
2020-10-22 15:48:24 +0100

    add client_secret download to config.py

--- bot/config.py
+++ bot/config.py
@@ -8,3 +8,6 @@ GOOGLE_SECRET_S3_KEY = os.getenv("GOOGLE_SECRET_S3_KEY")
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 # download client_secret.json for authenticating with Google API
+s3 = boto3.client('s3')
+with open('client_secret.json', 'wb') as f:
+    s3.download_fileobj(S3_BUCKET, GOOGLE_SECRET_S3_KEY, f)
